8e623ff928b99f0786ca622c38a0b05c0ef33925
jonnystv <jonathan@darefoundation.com>
2021-03-08 12:11:17 +0000

    Comment model created

--- news-cms-server/src/main/java/com/example/codeclan/newscmsserver/models/Comment.java
+++ news-cms-server/src/main/java/com/example/codeclan/newscmsserver/models/Comment.java
@@ -1,4 +1,51 @@
 package com.example.codeclan.newscmsserver.models;
 
 public class Comment {
+
+    private String commentTitle;
+
+    private String commentText;
+
+    private User commentAuthor;
+
+    private String commentDate;
+
+    public Comment(String commentTitle, String commentText, User commentAuthor, String commentDate) {
+        this.commentTitle = commentTitle;
+        this.commentText = commentText;
+        this.commentAuthor = commentAuthor;
+        this.commentDate = commentDate;
+    }
+
+    public String getCommentTitle() {
+        return commentTitle;
+    }
+
+    public void setCommentTitle(String commentTitle) {
+        this.commentTitle = commentTitle;
+    }
+
+    public String getCommentText() {
+        return commentText;
+    }
+
+    public void setCommentText(String commentText) {
+        this.commentText = commentText;
+    }
+
+    public User getCommentAuthor() {
+        return commentAuthor;
+    }
+
+    public void setCommentAuthor(User commentAuthor) {
+        this.commentAuthor = commentAuthor;
+    }
+
+    public String getCommentDate() {
+        return commentDate;
+    }
+
+    public void setCommentDate(String commentDate) {
+        this.commentDate = commentDate;
+    }
 }
